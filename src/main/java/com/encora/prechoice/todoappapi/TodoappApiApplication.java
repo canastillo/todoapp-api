5057165879e256002b791bba8db71b506710e93c
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2022-09-05 22:13:40 -0500

    Initialize project

---
+++ src/main/java/com/encora/prechoice/todoappapi/TodoappApiApplication.java
@@ -0,0 +1,13 @@
+package com.encora.prechoice.todoappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoappApiApplication.class, args);
+	}
+
+}
