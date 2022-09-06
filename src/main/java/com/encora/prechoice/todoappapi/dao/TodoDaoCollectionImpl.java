95de0b694acc7d6d3b90ba1b82695677bfa9f77c
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2022-09-06 09:55:13 -0500

    Added implementation for Todos in collection

---
+++ src/main/java/com/encora/prechoice/todoappapi/dao/TodoDaoCollectionImpl.java
@@ -0,0 +1,41 @@
+package com.encora.prechoice.todoappapi.dao;
+
+import com.encora.prechoice.todoappapi.domain.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TodoDaoCollectionImpl implements TodoDao{
+
+    @Override
+    public Todo save(Todo todo) {
+        return null;
+    }
+
+    @Override
+    public Optional<Todo> findById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Todo> findAll() {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void delete(Todo todo) {
+
+    }
+
+    @Override
+    public boolean existsById(Integer id) {
+        return false;
+    }
+}
