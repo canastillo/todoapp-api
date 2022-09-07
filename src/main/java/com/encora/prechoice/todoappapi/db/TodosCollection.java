7bc689553fc7d1f5a22740748a74c663b1ee778d
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2022-09-06 20:01:26 -0500

    Moved TodosCollection to /db

---
+++ src/main/java/com/encora/prechoice/todoappapi/db/TodosCollection.java
@@ -0,0 +1,43 @@
+package com.encora.prechoice.todoappapi.dao;
+
+import com.encora.prechoice.todoappapi.domain.Priority;
+import com.encora.prechoice.todoappapi.domain.Todo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodosCollection {
+    private List<Todo> todos = new ArrayList<>();
+    private int lastIndex = 0;
+
+    public TodosCollection() {
+        todos.add(new Todo(todos.size() + 1, "Finish react course", Priority.MEDIUM, LocalDateTime.now()));
+        todos.add(new Todo(todos.size() + 1, "Search for OS contributions", Priority.LOW, LocalDateTime.now(), LocalDate.of(2022, 10, 7)));
+        todos.add(new Todo(todos.size() + 1, "Write weekly essay", Priority.HIGH, LocalDateTime.now()));
+
+        lastIndex = todos.size();
+    }
+
+    @Override
+    public String toString() {
+        return "TodosCollection{" +
+                "todos=" + todos +
+                '}';
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public Todo addTodo(Todo todo) {
+        todo.setId(++lastIndex);
+        todos.add(todo);
+        return todo;
+    }
+
+    public int getSize() {
+        return todos.size();
+    }
+}
